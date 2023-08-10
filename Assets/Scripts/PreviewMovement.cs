3aa79981a280b36681d847eb62bdb005fae6d43e
Talha Ahmed <49395387+CommonCrayon@users.noreply.github.com>
2023-08-10 17:37:44 +1000

    Added CatDead CatRecovering and CatScared Animations

--- Assets/Scripts/PreviewMovement.cs
+++ Assets/Scripts/PreviewMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PreviewMoving : MonoBehaviour
 {
+    public bool CatScared = false;
+    public bool CatDead = false;
+    public bool CatRecovering = false;
     public int MoveCount = 0;
     private Animator animator;
 
@@ -17,25 +20,45 @@ public class PreviewMoving : MonoBehaviour
         if (MoveCount < 100)
         {
             transform.position += Vector3.up * 3 * Time.deltaTime;
-            animator.Play("Up");
+
+            if (CatScared) animator.Play("CatScared");
+            else if (CatDead) animator.Play("CatDead");
+            else if (CatRecovering) animator.Play("CatRecovering");
+            else animator.Play("Up");
+
             MoveCount++;
         }
         else if (MoveCount < 200)
         {
             transform.position += Vector3.right * 3 * Time.deltaTime;
-            animator.Play("Right");
+
+            if (CatScared) animator.Play("CatScared");
+            else if (CatDead) animator.Play("CatDead");
+            else if (CatRecovering) animator.Play("CatRecovering");
+            else animator.Play("Right");
+
             MoveCount++;
         }
         else if (MoveCount < 300)
         {
             transform.position += Vector3.down * 3 * Time.deltaTime;
-            animator.Play("Down");
+
+            if (CatScared) animator.Play("CatScared");
+            else if (CatDead) animator.Play("CatDead");
+            else if (CatRecovering) animator.Play("CatRecovering");
+            else animator.Play("Down");
+
             MoveCount++;
         }
         else if (MoveCount < 400)
         {
             transform.position += Vector3.left * 3 * Time.deltaTime;
-            animator.Play("Left");
+
+            if (CatScared) animator.Play("CatScared");
+            else if (CatDead) animator.Play("CatDead");
+            else if (CatRecovering) animator.Play("CatRecovering");
+            else animator.Play("Left");
+
             MoveCount++;
         }
         else
